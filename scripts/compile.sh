c3bcc360f3019e3c176f684a227151c8b8b04d82
narrative26 <adnan.kriaz@gmail.com>
2026-04-18 17:22:38 +0000

    added scripts

---
+++ scripts/compile.sh
@@ -0,0 +1,12 @@
+clear
+cd ../src/
+flex semaction.l
+g++ -o action.o lex.yy.c -ll
+./action.o < parser_empty.y
+rm lex.yy.c action.o
+bison -d -t -v parser.y
+flex lexer.l
+g++ -o WCTC.o parser.tab.c lex.yy.c node.cpp symbol_table.cpp tac.cpp x86.cpp main.cpp -ll
+./WCTC.o -i ../tests/test.java -t tac.txt -s asm.s
+dot -Tpng tree.gv -o AST.png
+rm tree.gv parser.output parser.tab.* lex.yy.c *.csv parser.y
